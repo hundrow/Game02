e57efeb814cfe8a9a5e39bce6917b3408bcc5659
hundrow <hundrow@gmail.com>
2024-06-15 11:07:51 -0700

    fix: Cleaning up the file to remove some test code.

--- MySprite2D.cs
+++ MySprite2D.cs
@@ -18,44 +18,32 @@ public partial class MySprite2D : Sprite2D
 		rightWheel = GetNode<Sprite2D>("rightWheel");
 	}
 	
-	public override void _Process(double delta)
-{
-	Rotation += _angularSpeed * (float)delta;
-	var velocity = Vector2.Up.Rotated(Rotation) * _speed;
-	Position += velocity * (float)delta;
-}
-	//public override void _Process(double delta){
-		//var direction = 0;
-		//if(Input.IsActionPressed("ui_left") && _currentWheelRotation > -_maxWheelRotation){
-			//float rotationThisFrame = _angularSpeed * (float)delta;
-			//leftWheel.Rotation -= rotationThisFrame;
-			//rightWheel.Rotation -= rotationThisFrame;
-			//_currentWheelRotation -= rotationThisFrame;
-		//}
-		//if(Input.IsActionPressed("ui_right") && _currentWheelRotation < _maxWheelRotation){
-			//float rotationThisFrame = _angularSpeed * (float)delta;
-			//leftWheel.Rotation += rotationThisFrame;
-			//rightWheel.Rotation += rotationThisFrame;
-			//_currentWheelRotation += rotationThisFrame;
-		//}
-//
-		//
-		//var velocity = Vector2.Zero;
-		//if(Input.IsActionPressed("ui_up")){
-			//velocity = Vector2.Up.Rotated(Rotation) * _speed;
-			//Rotation += _angularSpeed * _currentWheelRotation * (float)delta;
-		//}
-		//if(Input.IsActionPressed("ui_down")){
-			//velocity = Vector2.Down.Rotated(Rotation) * _speed;
-			//Rotation -= _angularSpeed * _currentWheelRotation * (float)delta;
-		//}
-		//Position += velocity * (float)delta;
-	//}
-	private void _on_button_pressed(){
-		SetProcess(!IsProcessing());
-	}
-	private void OnTimerTimeout(){
-		Visible = !Visible;
+	public override void _Process(double delta){
+		var direction = 0;
+		if(Input.IsActionPressed("ui_left") && _currentWheelRotation > -_maxWheelRotation){
+			float rotationThisFrame = _angularSpeed * (float)delta;
+			leftWheel.Rotation -= rotationThisFrame;
+			rightWheel.Rotation -= rotationThisFrame;
+			_currentWheelRotation -= rotationThisFrame;
+		}
+		if(Input.IsActionPressed("ui_right") && _currentWheelRotation < _maxWheelRotation){
+			float rotationThisFrame = _angularSpeed * (float)delta;
+			leftWheel.Rotation += rotationThisFrame;
+			rightWheel.Rotation += rotationThisFrame;
+			_currentWheelRotation += rotationThisFrame;
+		}
+
+		
+		var velocity = Vector2.Zero;
+		if(Input.IsActionPressed("ui_up")){
+			velocity = Vector2.Up.Rotated(Rotation) * _speed;
+			Rotation += _angularSpeed * _currentWheelRotation * (float)delta;
+		}
+		if(Input.IsActionPressed("ui_down")){
+			velocity = Vector2.Down.Rotated(Rotation) * _speed;
+			Rotation -= _angularSpeed * _currentWheelRotation * (float)delta;
+		}
+		Position += velocity * (float)delta;
 	}
 }
 
